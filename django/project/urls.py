e48ebb83af14bcb7a15dc32c4cda0f7e4d1bb768
Rich Hart <richard.hart@capco.com>
2021-03-14 14:39:21 -0400

    change home view

--- django/project/urls.py
+++ django/project/urls.py
@@ -17,11 +17,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from buckets.views import FileViewSet
+from django.shortcuts import redirect
+
 
 router = DefaultRouter()
 router.register(r'files', FileViewSet)
 
+def home(request):
+    return redirect('api-root')
+
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
