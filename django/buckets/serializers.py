8f97be7d90588523eb28c4e49256b9e5ad9c9cfe
Rich Hart <richard.hart@capco.com>
2021-03-14 15:16:28 -0400

    serve object id

--- django/buckets/serializers.py
+++ django/buckets/serializers.py
@@ -19,6 +19,7 @@ class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = File
         fields = (
+            'id',
             'object_id',
             'instance',
             'headers',
